d76568ed2992e1a78b9fd3ebffe01844a87ecc0f
adama seye <seyeadam1@gmail.com>
2023-11-25 23:39:49 +0100

    complete test karate

--- src/main/java/com/sid/gl/testkarate/controller/UserController.java
+++ src/main/java/com/sid/gl/testkarate/controller/UserController.java
@@ -4,11 +4,9 @@ import com.sid.gl.testkarate.model.User;
 import com.sid.gl.testkarate.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -22,5 +20,15 @@ public class UserController {
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
         Optional<User> user = userRepository.findById(id);
         return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+        return  ResponseEntity.ok(userRepository.save(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> allUsers(){
+        return ResponseEntity.ok(userRepository.findAll());
     }
 }
--- src/main/resources/UserApiTest.feature
+++ src/main/resources/UserApiTest.feature
@@ -1,15 +1,30 @@
 Feature: Testing User API with Database Interaction
 
   Background:
-    * url 'http://localhost:8080'
+    * url 'http://localhost:8080/users'
 
   Scenario: Retrieve user by ID
-    Given path '/users/1'
+    Given path '/1'
     When method get
     Then status 200
     And match response == { id: 1, username: 'testuser', email: 'test@example.com' }
 
   Scenario: Verify user not found
-    Given path '/users/100'
+    Given path '/100'
     When method get
     Then status 404
+
+
+  Scenario: Create a user
+    Given request { "id": 2,"username":"adama", "email": "adama@gmail.com" }
+    When method post
+    Then status 200
+    And match response.username == 'adama'
+    And match response.email == 'adama@gmail.com'
+
+
+  Scenario: Get all user
+    Given path '/'
+    When method get
+    Then status 200
+    And match response == [{ id: 1, username: 'testuser', email: 'test@example.com' },{ "id": 2,"username":"adama", "email": "adama@gmail.com" }]
